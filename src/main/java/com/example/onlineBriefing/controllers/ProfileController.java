3defd254baf92f3646cb66c2802d73819f6af5b0
alinafffff <alfasar1@yandex.ru>
2024-05-14 12:35:54 +0400

    метод поиска всех профилей

--- src/main/java/com/example/onlineBriefing/controllers/ProfileController.java
+++ src/main/java/com/example/onlineBriefing/controllers/ProfileController.java
@@ -1,10 +1,15 @@
 package com.example.onlineBriefing.controllers;
 
+import com.example.onlineBriefing.models.Profile;
 import com.example.onlineBriefing.services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -14,5 +19,9 @@ public class ProfileController {
     @Autowired
     private ProfileService profileService;
 
-
+    @GetMapping("/getAllProfiles")
+    public ResponseEntity<List<Profile>> getAllProfiles() {
+        List<Profile> profiles = profileService.getAllProfiles();
+        return ResponseEntity.ok(profiles);
+    }
 }
